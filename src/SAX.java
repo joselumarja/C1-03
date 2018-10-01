ed6a0f5182a8a1fe5bb1d70486d9dcc1a3540995
joselumarja <34023329+joselumarja@users.noreply.github.com>
2018-10-01 14:13:13 +0200

    Mas SAX

--- src/SAX.java
+++ src/SAX.java
@@ -6,39 +6,69 @@ import java.util.*;
 import java.io.*;
 
 
-public class SAX {
+public class SAX extends DefaultHandler {
 
-		static public void main(String[] args) {
-			
-			String filename = null;
+	private Hashtable tags;
 
-		    for (int i = 0; i < args.length; i++) {
-		        filename = args[i];
-		        if (i != args.length - 1) {
-		            usage();
-		        }
-		    }
+	static public void main(String[] args) {
 
-		    if (filename == null) {
-		        usage();
-		    } 
+		String filename = null;
+
+		for (int i = 0; i < args.length; i++) {
+			filename = args[i];
+			if (i != args.length - 1) {
+				usage();
+			}
 		}
-		
-		private static void usage() {
-		    System.err.println("Usage: SAXLocalNameCount <file.xml>");
-		    System.err.println("       -usage or -help = this message");
-		    System.exit(1);
-		}
-		
-		private static String convertToFileURL(String filename) {
-	        String path = new File(filename).getAbsolutePath();
-	        if (File.separatorChar != '/') {
-	            path = path.replace(File.separatorChar, '/');
-	        }
 
-	        if (!path.startsWith("/")) {
-	            path = "/" + path;
-	        }
-	        return "file:" + path;
-	    }
+		if (filename == null) {
+			usage();
+		}
+	}
+
+	private static void usage() {
+		System.err.println("Usage: SAXLocalNameCount <file.xml>");
+		System.err.println("       -usage or -help = this message");
+		System.exit(1);
+	}
+
+	private static String convertToFileURL(String filename) {
+		String path = new File(filename).getAbsolutePath();
+		if (File.separatorChar != '/') {
+			path = path.replace(File.separatorChar, '/');
+		}
+
+		if (!path.startsWith("/")) {
+			path = "/" + path;
+		}
+		return "file:" + path;
+	}
+
+	public void startDocument() throws SAXException {
+		tags = new Hashtable();
+	}
+
+	public void endDocument() throws SAXException {
+		Enumeration e = tags.keys();
+		while (e.hasMoreElements()) {
+			String tag = (String) e.nextElement();
+			int count = ((Integer) tags.get(tag)).intValue();
+			System.out.println("Local Name \"" + tag + "\" occurs " + count + " times");
+		}
+	}
+
+	public void startElement(String namespaceURI, String localName, String qName, Attributes atts) throws SAXException {
+
+		String key = localName;
+		Object value = tags.get(key);
+
+		if (value == null) {
+			tags.put(key, new Integer(1));
+		} else {
+			int count = ((Integer) value).intValue();
+			count++;
+			tags.put(key, new Integer(count));
+		}
+	}
+
 }
