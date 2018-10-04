853cc515d412e665673ae25a8f0bc1b01a903fd7
RubenMarVil <43539841+RubenMarVil@users.noreply.github.com>
2018-10-04 16:27:38 +0200

    Prueba 2

--- src/main.java
+++ src/main.java
@@ -8,7 +8,7 @@ public class main {
 		Scanner sc = new Scanner(System.in);
 		
 		//introducir aqui ruta al fichero del grafo
-		String ruta = "Villar del Pozo.graphml";
+		String ruta = "Pueblo.graphml";
 		
 		Grafo g = new Grafo(ruta);
 		int osmId;
