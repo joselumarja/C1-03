4d1e11f8453ecdf2bfdfecdb0c08795a5f010ce3
joselumarja <34023329+joselumarja@users.noreply.github.com>
2018-10-06 11:40:51 +0200

    .

--- src/main.java
+++ src/main.java
@@ -8,7 +8,7 @@ public class main {
 		Scanner sc = new Scanner(System.in);
 		
 		//introducir aqui ruta al fichero del grafo
-		String ruta = "";
+		String ruta = "Villar del Pozo.graphml";
 		
 		Grafo g = new Grafo(ruta);
 		int osmId;
